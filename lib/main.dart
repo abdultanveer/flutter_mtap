611d616b4d1bc2d341599c66d65eedf740526d87
abdul <abdul.tanveer@gmail.com>
2021-08-09 18:07:56 +0530

    building a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,40 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'my first app',
-          style: TextStyle(
-            fontSize: 20.0,
-            letterSpacing: 2.0,
-            fontFamily: 'IndieFlower',
-            color: Colors.red,
-          ),
-        ),
-        centerTitle: true,
-      ),
-          body: Center(
-              child: Text('welcome to moneytap')
-          ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.blueAccent,
-            child: Text('click me'),
-            onPressed: () {  },
+        home: MyApp()
+));
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'my first app',
+            style: TextStyle(
+              fontSize: 20.0,
+              letterSpacing: 2.0,
+              fontFamily: 'IndieFlower',
+              color: Colors.red,
+            ),
           ),
-    )));
+          centerTitle: true,
+        ),
+        body: Center(
+            child: RaisedButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.airport_shuttle),
+                label: Text('mail me'))
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          child: Text('press'),
+          onPressed: () {  },
+
+        ),
+      );
+  }
+}
